e8cad994680bd5dc26180da69b3586d1d86c4424
fabrisdev <ezepuntocom@hotmail.com>
2026-01-15 22:42:23 -0300

    added warnAndCrash helper

--- environment.ts
+++ environment.ts
@@ -1,10 +1,12 @@
 export function token() {
 	const token = process.env.BOT_TOKEN;
-	if (token === undefined) {
-		console.log(
-			"Missing discord bot token. Did you made sure to create a .env and include all needed fields?",
-		);
-		process.exit(1);
-	}
+	if (token === undefined) warnAndCrash("token");
 	return token;
+}
+
+function warnAndCrash(field: string): never {
+	console.error(
+		`Missing ${field}. Did you made sure to create a .env and populate all needed fields?`,
+	);
+	process.exit(1);
 }
