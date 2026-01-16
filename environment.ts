51c15d2efbff9c19fa2a991644a4839512437718
fabrisdev <ezepuntocom@hotmail.com>
2026-01-15 22:44:14 -0300

    Added new helpers

--- environment.ts
+++ environment.ts
@@ -4,6 +4,18 @@ export function token() {
 	return token;
 }
 
+export function clientId() {
+	const clientId = process.env.CLIENT_ID;
+	if (clientId === undefined) warnAndCrash("client id");
+	return clientId;
+}
+
+export function guildId() {
+	const guildId = process.env.GUILD_ID;
+	if (guildId === undefined) warnAndCrash("guild id");
+	return guildId;
+}
+
 function warnAndCrash(field: string): never {
 	console.error(
 		`Missing ${field}. Did you made sure to create a .env and populate all needed fields?`,
