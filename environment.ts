97c5e44df0994e8e763ca351a2030e1268da9f2b
fabrisdev <ezepuntocom@hotmail.com>
2026-01-15 22:39:40 -0300

    removed verbosity

--- environment.ts
+++ environment.ts
@@ -1,5 +1,5 @@
 export function token() {
-	const token = process.env.DISCORD_BOT_TOKEN;
+	const token = process.env.BOT_TOKEN;
 	if (token === undefined) {
 		console.log(
 			"Missing discord bot token. Did you made sure to create a .env and include all needed fields?",
