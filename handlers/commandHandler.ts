aa9b4868d587cf8a19f5e3369ab16e66ec357740
fabrisdev <ezepuntocom@hotmail.com>
2026-01-15 22:06:56 -0300

    Added command handling

--- handlers/commandHandler.ts
+++ handlers/commandHandler.ts
@@ -1,1 +1,17 @@
-export function getCommands() {}
+import fs from "node:fs/promises";
+import path from "node:path";
+
+const EXPECTED_EXTENSION = ".command.ts";
+const EXPECTED_FOLDERNAME = "commands";
+
+export async function fetchCommands() {
+	const commands = [];
+	const files = await fs.readdir(EXPECTED_FOLDERNAME);
+	for (const file of files) {
+		if (!file.endsWith(EXPECTED_EXTENSION)) continue;
+		const importedFile = await import(path.join(EXPECTED_FOLDERNAME, file));
+		const command = importedFile.default;
+		commands.push(command);
+	}
+	return commands;
+}
