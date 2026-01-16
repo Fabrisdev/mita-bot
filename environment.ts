b0717f1ca1336e7271ef96f372a7835896c7b01a
fabrisdev <ezepuntocom@hotmail.com>
2026-01-15 22:36:30 -0300

    added environment reader helper

---
+++ environment.ts
@@ -0,0 +1,10 @@
+export function token() {
+	const token = process.env.DISCORD_BOT_TOKEN;
+	if (token === undefined) {
+		console.log(
+			"Missing discord bot token. Did you made sure to create a .env and include all needed fields?",
+		);
+		process.exit(1);
+	}
+	return token;
+}
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { Client, Events, GatewayIntentBits } from "discord.js";
+import { token } from "./environment";
 import { fetchCommands } from "./handlers/commandHandler";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -35,11 +36,4 @@ client.on(Events.InteractionCreate, async (interaction) => {
 	}
 });
 
-const token = process.env.DISCORD_BOT_TOKEN;
-if (token === undefined) {
-	console.log(
-		"Missing discord bot token. Did you made sure to create a .env and include all needed fields?",
-	);
-	process.exit(1);
-}
-client.login(token);
+client.login(token());
