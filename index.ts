59a619e78339168cd8cb76d1c5c9947bc202dea0
fabrisdev <ezepuntocom@hotmail.com>
2026-01-15 21:44:23 -0300

    Added basic start

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.once(Events.ClientReady, (client) => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+const token = process.env.DISCORD_BOT_TOKEN;
+if (token === undefined) {
+	console.log(
+		"Missing discord bot token. Did you made sure to create a .env and include all needed fields?",
+	);
+	process.exit(1);
+}
+client.login(token);
