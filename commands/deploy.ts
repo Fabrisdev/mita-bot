3827a71825a5fa99246124b3a45569fae4adb513
fabrisdev <ezepuntocom@hotmail.com>
2026-01-16 22:16:38 -0300

    Updated deployer to work with options

--- commands/deploy.ts
+++ commands/deploy.ts
@@ -3,11 +3,17 @@ import { clientId, guildId, token } from "../environment";
 import { fetchCommands } from "./handler";
 
 const commands = await fetchCommands();
-const commandsData = Array.from(commands).map((command) =>
-	new SlashCommandBuilder()
-		.setName(command[0])
-		.setDescription(command[1].description),
-);
+const commandsData = Array.from(commands).map(([name, command]) => {
+	const builder = new SlashCommandBuilder()
+		.setName(name)
+		.setDescription(command.description);
+	if (!command.options) return builder;
+
+	const json = builder.toJSON();
+	//@ts-expect-error This is not an actual error, rather Typescript not being able to confirm that the two types are actually the same.
+	json.options = command.options;
+	return json;
+});
 
 const rest = new REST().setToken(token());
 console.log(`Started refreshing slash commands.`);
