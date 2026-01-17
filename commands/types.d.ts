16ec0f3fefee0f9764c139c786cfa56c555c3326
fabrisdev <ezepuntocom@hotmail.com>
2026-01-16 22:43:18 -0300

    new permissions field

--- commands/types.d.ts
+++ commands/types.d.ts
@@ -1,10 +1,12 @@
 import type {
 	ApplicationCommandOptionData,
 	ChatInputCommandInteraction,
+	PermissionFlagBits,
 } from "discord.js";
 
 export type Command = {
 	description: string;
 	options?: ApplicationCommandOptionData[];
+	permissions: PermissionFlagBits[];
 	run: (interaction: ChatInputCommandInteraction) => Promise<void>;
 };
