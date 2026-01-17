25fb7bc60e8dbe6bd6eef0883f20420957786e0b
fabrisdev <ezepuntocom@hotmail.com>
2026-01-17 04:46:08 -0300

    using db api in alert

--- commands/alert.ts
+++ commands/alert.ts
@@ -1,8 +1,11 @@
 import type { TextChannel } from "discord.js";
 import { client } from "../client";
+import { getAlertsChannel } from "../db";
 
-export async function alert(channelId: string, message: string) {
-	const channel = await client.channels.fetch(channelId);
+export async function alert(guildId: string, message: string) {
+	const alertsChannel = await getAlertsChannel(guildId);
+	if (!alertsChannel) return;
+	const channel = await client.channels.fetch(alertsChannel);
 	if (channel == null) return; // in the future, notify admins that the alerts channel no longer exists
 	if (!channel.isTextBased()) return;
 	await (channel as TextChannel).send(message);
