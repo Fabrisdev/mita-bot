11ea822b0cf9cacccfae54119582cc52784de1dc
fabrisdev <ezepuntocom@hotmail.com>
2026-01-17 15:09:20 -0300

    Basic ticket command

---
+++ commands/ticket.command.ts
@@ -0,0 +1,42 @@
+import { ChannelType, type Guild, MessageFlags } from "discord.js";
+import type { Command } from "./types";
+
+export default {
+	description: "Create a ticket to speak with our staff members",
+	environment: "guild",
+	permissions: [],
+	run: async (interaction) => {
+		const category = await findTicketsCategory(interaction.guild);
+		interaction.guild.channels.create({
+			name: crypto.randomUUID(),
+			type: ChannelType.GuildText,
+			parent: category.id,
+		});
+		await interaction.reply({
+			content: `A ticket channel has been created for you at <#${category.id}>`,
+			flags: MessageFlags.Ephemeral,
+		});
+	},
+} satisfies Command<"guild">;
+
+async function findTicketsCategory(guild: Guild) {
+	const channels = await guild.channels.fetch();
+	const category = channels.find(
+		(channel) =>
+			channel?.type === ChannelType.GuildCategory && channel.name === "Tickets",
+	);
+	if (!category) {
+		const newCategory = await guild.channels.create({
+			name: "Tickets",
+			type: ChannelType.GuildCategory,
+			permissionOverwrites: [
+				{
+					id: guild.id, // @everyone
+					deny: ["ViewChannel"],
+				},
+			],
+		});
+		return newCategory;
+	}
+	return category;
+}
