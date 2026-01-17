44eab546a9159518b2381c88e0c37d7cd96b2495
fabrisdev <ezepuntocom@hotmail.com>
2026-01-17 16:24:57 -0300

    new warn command

---
+++ commands/warn.command.ts
@@ -0,0 +1,41 @@
+import { ApplicationCommandOptionType, PermissionFlagsBits } from "discord.js";
+import { addToUserHistory } from "../db";
+import { showAlert } from "./alert";
+import type { Command } from "./types";
+
+export default {
+	description: "Warn a user",
+	environment: "guild",
+	permissions: [PermissionFlagsBits.MuteMembers],
+	options: [
+		{
+			name: "user",
+			description: "User to warn",
+			type: ApplicationCommandOptionType.User,
+			required: true,
+		},
+		{
+			name: "reason",
+			description: "Reason to warn",
+			type: ApplicationCommandOptionType.String,
+		},
+	],
+	run: async (interaction) => {
+		const user = interaction.options.getUser("user", true);
+		const reason =
+			interaction.options.getString("reason") ?? "No reason specified";
+		await addToUserHistory(interaction.guild.id, user.id, {
+			at: Date.now(),
+			moderatorId: interaction.user.id,
+			reason,
+			type: "warn",
+		});
+		await interaction.reply(
+			`The user ${user.tag} has been warned with the reason: ${reason}`,
+		);
+		await showAlert(
+			interaction.guild.id,
+			`Moderator ${interaction.user.tag} has warned ${user.tag} with the reason: ${reason}`,
+		);
+	},
+} satisfies Command<"guild">;
