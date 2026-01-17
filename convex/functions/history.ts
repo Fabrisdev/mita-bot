147446502b5a6e8717a9e2f9c444c181af776efd
fabrisdev <ezepuntocom@hotmail.com>
2026-01-17 15:55:43 -0300

    added adding to history

--- convex/functions/history.ts
+++ convex/functions/history.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { query } from "../_generated/server";
+import { mutation, query } from "../_generated/server";
 
 export const getUserHistory = query({
 	args: {
@@ -12,5 +12,41 @@ export const getUserHistory = query({
 			.withIndex("by_user_and_guild", (q) => q.eq("guildId", args.guildId))
 			.filter((q) => q.eq(q.field("userId"), args.userId))
 			.first();
+	},
+});
+
+export const addToUserHistory = mutation({
+	args: {
+		guildId: v.string(),
+		userId: v.string(),
+		incident: v.object({
+			type: v.union(
+				v.literal("ban"),
+				v.literal("unban"),
+				v.literal("kick"),
+				v.literal("mute"),
+				v.literal("warn"),
+			),
+			at: v.number(),
+			moderatorId: v.string(),
+		}),
+	},
+	handler: async (ctx, args) => {
+		const history = await ctx.db
+			.query("history")
+			.withIndex("by_user_and_guild", (q) => q.eq("guildId", args.guildId))
+			.filter((q) => q.eq(q.field("userId"), args.userId))
+			.first();
+		if (history) {
+			await ctx.db.patch(history._id, {
+				history: [...history.history, args.incident],
+			});
+			return;
+		}
+		await ctx.db.insert("history", {
+			guildId: args.guildId,
+			userId: args.userId,
+			history: [args.incident],
+		});
 	},
 });
