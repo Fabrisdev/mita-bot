61234e5c1dc23178d02bb0fec1a1e9187dab64c7
fabrisdev <ezepuntocom@hotmail.com>
2026-01-17 16:12:13 -0300

    added history command

---
+++ commands/history.command.ts
@@ -0,0 +1,25 @@
+import { PermissionFlagsBits } from "discord.js";
+import { getUserHistory } from "../db";
+import type { Command } from "./types";
+
+export default {
+	description: "Get an user's history",
+	environment: "guild",
+	permissions: [PermissionFlagsBits.MuteMembers],
+	run: async (interaction) => {
+		const history = await getUserHistory(
+			interaction.guild.id,
+			interaction.user.id,
+		);
+		if (history === undefined) {
+			interaction.reply("User's history is clean.");
+			return;
+		}
+
+		const message = history.map((incident) => {
+			const date = new Date(incident.at).toLocaleString();
+			return `**${incident.type.toUpperCase()}**: Issued by moderator <@${incident.moderatorId}> at ${date} with the reason: ${incident.reason}`;
+		});
+		await interaction.reply(message.join("\n"));
+	},
+} satisfies Command<"guild">;
