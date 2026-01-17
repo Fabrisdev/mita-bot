da9b7cc725c82d4bdabffbd583b4ba134d2245f1
fabrisdev <ezepuntocom@hotmail.com>
2026-01-17 16:13:56 -0300

    fixed checking own person's history instead of the one they want

--- commands/history.command.ts
+++ commands/history.command.ts
@@ -1,4 +1,4 @@
-import { PermissionFlagsBits } from "discord.js";
+import { ApplicationCommandOptionType, PermissionFlagsBits } from "discord.js";
 import { getUserHistory } from "../db";
 import type { Command } from "./types";
 
@@ -6,11 +6,17 @@ export default {
 	description: "Get an user's history",
 	environment: "guild",
 	permissions: [PermissionFlagsBits.MuteMembers],
+	options: [
+		{
+			name: "user",
+			description: "User to check",
+			type: ApplicationCommandOptionType.User,
+			required: true,
+		},
+	],
 	run: async (interaction) => {
-		const history = await getUserHistory(
-			interaction.guild.id,
-			interaction.user.id,
-		);
+		const user = interaction.options.getUser("user", true);
+		const history = await getUserHistory(interaction.guild.id, user.id);
 		if (history === undefined) {
 			interaction.reply("User's history is clean.");
 			return;
