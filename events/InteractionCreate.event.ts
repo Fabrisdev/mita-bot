1260a8b70ab7af9c9da85ea0ddc888a19e9968b6
fabrisdev <ezepuntocom@hotmail.com>
2026-01-16 21:15:20 -0300

    separated interactioncreate into another file

--- events/InteractionCreate.event.ts
+++ events/InteractionCreate.event.ts
@@ -1,1 +1,32 @@
-export default {};
+import type { CacheType, Interaction } from "discord.js";
+import { fetchCommands } from "../handlers/commandHandler";
+
+const commands = await fetchCommands();
+
+export default async (interaction: Interaction<CacheType>) => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = commands.get(interaction.commandName);
+	if (command === undefined) {
+		console.error(
+			`Interaction ${interaction.commandName} was run, but there were no commands found matching it. More info below:`,
+		);
+		console.error("INTERACTION COMMAND NAME:", interaction.commandName);
+		console.error("COMMANDS AVAILABLE:", commands);
+		return;
+	}
+	try {
+		await command.run(interaction);
+	} catch (error) {
+		console.error(
+			`An error ocurred whilst executing ${interaction.commandName} interaction. More info below:`,
+		);
+		console.error(error);
+		const errorMessage =
+			"There was an error while executing this command. We've been notified about and are working on fixing it.";
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp(errorMessage);
+			return;
+		}
+		await interaction.reply(errorMessage);
+	}
+};
