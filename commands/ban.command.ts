4c95f908cd33a57b1bc0394f2d22133649554866
fabrisdev <ezepuntocom@hotmail.com>
2026-01-16 22:55:17 -0300

    Finished adding all necessary checks before being able to ban

--- commands/ban.command.ts
+++ commands/ban.command.ts
@@ -24,8 +24,36 @@ export default {
 			interaction.options.getString("reason") ?? "No reason specified";
 
 		if (user.id === interaction.client.user.id) {
-			interaction.reply({
+			await interaction.reply({
 				content: "No! :(",
+			});
+			return;
+		}
+
+		if (!interaction.guild) {
+			await interaction.reply({
+				content: "This command can only be run inside the server.",
+				ephemeral: true,
+			});
+			return;
+		}
+
+		const member = await interaction.guild.members
+			.fetch(user.id)
+			.catch(() => null);
+
+		if (!member) {
+			await interaction.reply({
+				content: "That user does not seem to be on the server.",
+				ephemeral: true,
+			});
+			return;
+		}
+
+		if (!member.bannable) {
+			await interaction.reply({
+				content: "I can't ban that user!",
+				ephemeral: true,
 			});
 			return;
 		}
