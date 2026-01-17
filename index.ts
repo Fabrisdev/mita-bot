c21b894be4545c2985f45c810394b388988a3b8a
fabrisdev <ezepuntocom@hotmail.com>
2026-01-16 21:44:18 -0300

    removed unnecessary newlines

--- index.ts
+++ index.ts
@@ -3,7 +3,5 @@ import { token } from "./environment";
 import { registerEvents } from "./events/register";
 
 export const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
 await registerEvents();
-
 client.login(token());
