bcc18d302e8670fa5a286e9df9f532248b5416e2
fabrisdev <ezepuntocom@hotmail.com>
2026-01-17 01:24:00 -0300

    reworked Command type to set environment

--- commands/types.d.ts
+++ commands/types.d.ts
@@ -1,12 +1,22 @@
 import type {
 	ApplicationCommandOptionData,
 	ChatInputCommandInteraction,
+	Guild,
 	PermissionFlagBits,
 } from "discord.js";
 
-export type Command = {
+type Environment = "guild" | "dm" | "both";
+
+export type Command<E extends Environment> = {
 	description: string;
 	options?: ApplicationCommandOptionData[];
 	permissions: PermissionFlagBits[];
-	run: (interaction: ChatInputCommandInteraction) => Promise<void>;
+	environment: E;
+	run: (
+		interaction: E extends "guild"
+			? ChatInputCommandInteraction & { guild: Guild }
+			: E extends "dm"
+				? ChatInputCommandInteraction & { guild: null }
+				: ChatInputCommandInteraction,
+	) => Promise<void>;
 };
