a3bd0a59d9fd5b24257fc0aab303e09f747fda68
fabrisdev <ezepuntocom@hotmail.com>
2026-01-16 21:21:28 -0300

    Added events handler

---
+++ handlers/eventsHandler.ts
@@ -0,0 +1,21 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import type { Event } from "../events/types";
+
+const EXPECTED_EXTENSION = ".event.ts";
+const EXPECTED_FOLDERNAME = "events";
+
+export async function fetchEvents() {
+	const events = new Map<string, Event>();
+	const files = await fs.readdir(EXPECTED_FOLDERNAME);
+	for (const file of files) {
+		if (!file.endsWith(EXPECTED_EXTENSION)) continue;
+		const importedFile = await import(
+			path.join("..", EXPECTED_FOLDERNAME, file)
+		);
+		const event = importedFile.default;
+		const name = file.slice(0, -EXPECTED_EXTENSION.length);
+		events.set(name, event);
+	}
+	return events;
+}
