48994ad43a74971b4d81258018d4d4da283f01bf
fabrisdev <ezepuntocom@hotmail.com>
2026-01-19 16:28:00 -0300

    example send message

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,34 @@
+import { ChannelType } from "discord.js";
 import Elysia from "elysia";
-import { apiPort } from "../environment";
+import z from "zod";
+import { client } from "../client";
+import { apiPort, apiSecret } from "../environment";
+
+const sendMessageSchema = z.object({
+	channelId: z.string(),
+	message: z.string(),
+	secret: z.string(),
+});
 
 const elysia = new Elysia()
 	.get("/api/ok", "")
-	.post("/api/send-message", ({ body }) => {});
+	.post("/api/send-message", async ({ body }) => {
+		const parsed = sendMessageSchema.safeParse(body);
+		if (parsed.error || parsed.data.secret !== apiSecret())
+			return {
+				status: 400,
+			};
+		const { channelId, message } = parsed.data;
+
+		try {
+			const channel = await client.channels.fetch(channelId);
+			if (!channel || channel.type !== ChannelType.GuildText)
+				return { status: 400 };
+			channel.send(message);
+		} catch {
+			return { status: 400 };
+		}
+	});
 
 export function startApiService() {
 	const port = apiPort();
