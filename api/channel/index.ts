b4f6389c0f21a9308a91ced2c5cdaf49fe022d83
fabrisdev <ezepuntocom@hotmail.com>
2026-01-22 20:41:40 -0300

    added guard for proper typing

--- api/channel/index.ts
+++ api/channel/index.ts
@@ -1,11 +1,14 @@
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
 import { ChannelModel } from "./model";
 import { ChannelService } from "./service";
 
 export const ChannelController = new Elysia({ prefix: "/channel" })
+	.guard({
+		params: t.Object({
+			guildId: t.String(),
+		}),
+	})
 	.post("/send", ({ body }) => ChannelService.send(body), {
 		body: ChannelModel.SendBody,
 	})
-	.get("/:guildId", ({ params }) =>
-		ChannelService.getAll({ guildId: params.guildId }),
-	);
+	.get("/", ({ params }) => ChannelService.getAll({ guildId: params.guildId }));
