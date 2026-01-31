c580c9d3ef017097f77e54233c874e0a3a0f806a
fabrisdev <ezepuntocom@hotmail.com>
2026-01-30 23:12:14 -0300

    more info to display

--- api/guild/service.ts
+++ api/guild/service.ts
@@ -6,13 +6,16 @@ export namespace GuildService {
 	export async function getAll({ userId }: GuildModel.GetAll) {
 		const guilds = await Promise.all(
 			client.guilds.cache.map(async (guild) => {
-				const isMember = await guild.members.fetch(userId).catch(() => false);
-				if (!isMember) return null;
+				const member = await guild.members.fetch(userId).catch(() => null);
+				if (!member) return null;
 				if (!isAdminAt(guild.id, userId)) return null;
 				return {
 					name: guild.name,
 					id: guild.id,
 					icon: guild.iconURL(),
+					banner: guild.bannerURL(),
+					memberCount: guild.memberCount,
+					highestRole: member.roles.highest.name,
 				};
 			}),
 		);
