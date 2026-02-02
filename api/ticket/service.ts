bb4b497c9c5d8cbe0f64d7446a6b474b3a05d874
fabrisdev <ezepuntocom@hotmail.com>
2026-02-01 21:57:04 -0300

    implement get all

--- api/ticket/service.ts
+++ api/ticket/service.ts
@@ -1,5 +1,8 @@
+import { Ticket } from "../../db";
 import type { TicketModel } from "./model";
 
 export namespace TicketService {
-	export async function getAll({ guildId }: TicketModel.GetAll) {}
+	export async function getAll({ guildId }: TicketModel.GetAll) {
+		return Ticket.all(guildId);
+	}
 }
