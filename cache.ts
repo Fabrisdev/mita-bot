2ec0f5d88134ce3570265c3759036de1006dcead
fabrisdev <ezepuntocom@hotmail.com>
2026-02-03 22:34:32 -0300

    typed cache

--- cache.ts
+++ cache.ts
@@ -1,10 +1,21 @@
-const cache: Record<string, unknown> = {};
+import type { api } from "./convex/_generated/api";
+
+type Cache = {
+	tickets: Ticket[];
+};
+
+const cache: Cache = {
+	tickets: [],
+};
 
 export namespace Cache {
-	export function store(key: string, value: unknown) {
+	export function store<K extends keyof Cache>(key: K, value: Cache[K]) {
 		cache[key] = value;
 	}
-	export function read(key: string) {
+	export function read<K extends keyof Cache>(key: K) {
 		return cache[key];
 	}
 }
+
+type Ticket =
+	(typeof api.functions.tickets.getTicketsFromGuild._returnType)[number];
