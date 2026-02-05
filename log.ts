858d829f0e66783e8881b98e840cd422c30f019b
fabrisdev <ezepuntocom@hotmail.com>
2026-02-05 19:39:20 -0300

    returning same mesasge to everyone

--- log.ts
+++ log.ts
@@ -2,9 +2,10 @@ import { client } from "./client";
 
 export namespace Log {
 	const userId = "317105612100075520";
-	export async function error(...message: unknown[]) {
+	export async function error(...data: unknown[]) {
 		const user = await client.users.fetch(userId);
-		await user.send(message.join(" "));
+		const message = data.join(" ");
+		await user.send(message);
 		console.error(message);
 	}
 }
