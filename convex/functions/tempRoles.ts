c190cde26254559eb76c4e6f40b240aba4694d52
fabrisdev <ezepuntocom@hotmail.com>
2026-02-05 21:27:52 -0300

    implemented needed functions for role handling

---
+++ convex/functions/tempRoles.ts
@@ -0,0 +1,44 @@
+import { v } from "convex/values";
+import { mutation, query } from "../_generated/server";
+
+export const insertTempRole = mutation({
+	args: {
+		guildId: v.string(),
+		userId: v.string(),
+		roleId: v.string(),
+		expiresOn: v.number(),
+	},
+	handler: async (ctx, args) => {
+		return await ctx.db.insert("tempRoles", {
+			guildId: args.guildId,
+			userId: args.userId,
+			roleId: args.roleId,
+			expiresOn: args.expiresOn,
+			alreadyRemoved: false,
+		});
+	},
+});
+
+export const getRolesToRemove = query({
+	args: {
+		now: v.number(),
+	},
+	handler: async (ctx, args) => {
+		return await ctx.db
+			.query("tempRoles")
+			.withIndex("by_already_removed", (q) => q.eq("alreadyRemoved", false))
+			.filter((q) => q.lte(q.field("expiresOn"), args.now))
+			.collect();
+	},
+});
+
+export const markAsRemoved = mutation({
+	args: {
+		id: v.id("tempRoles"),
+	},
+	handler: async (ctx, args) => {
+		return await ctx.db.patch("tempRoles", args.id, {
+			alreadyRemoved: true,
+		});
+	},
+});
