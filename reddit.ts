d6f3d41a1e8d584f2aad8619564e8f991b436474
fabrisdev <ezepuntocom@hotmail.com>
2026-02-22 02:31:13 -0300

    sending posts to all servers

--- reddit.ts
+++ reddit.ts
@@ -1,44 +1,62 @@
 import { EmbedBuilder } from "discord.js";
 import { client } from "./client";
-import type { RedditData } from "./reddit.types";
+import { Settings } from "./db";
+import { Log } from "./log";
+import type { Data2, RedditData } from "./reddit.types";
 
 export async function publishRedditPosts() {
-	const posts = await fetchPosts();
-	if (posts === null) return;
-	const channel = await client.channels.fetch("1474975172565143713");
-	if (channel === null)
-		throw new Error("Failed fetching reddit Discord channel");
-	if (!channel.isSendable()) throw new Error("Can't send there!");
+	const embeds = await getEmbeds();
+	if (embeds === null) return;
+	const channelsInfo = await Settings.getGuildsWithSetRedditFeedChannel();
+	for (const channelInfo of channelsInfo) {
+		const channel = await client.channels
+			.fetch(channelInfo.reddit_feed_channel_id)
+			.catch(() => null);
+		if (channel === null || !channel.isSendable()) return;
+		for (const embed of embeds) {
+			await channel.send({
+				embeds: [embed],
+			});
+		}
+	}
+}
 
+async function getEmbeds() {
+	const posts = await fetchPosts();
+	if (posts === null) return null;
+	const embeds: EmbedBuilder[] = [];
 	for (const item of posts.data.children) {
 		const d = item.data;
-
-		const image =
-			d.post_hint === "image"
-				? d.url
-				: d.preview?.images?.[0]?.source?.url?.replaceAll("&amp;", "&") || null;
-
-		const embed = new EmbedBuilder()
-			.setTitle(d.title)
-			.setURL(`https://reddit.com${d.permalink}`)
-			.setDescription(d.selftext ? d.selftext.slice(0, 2000) : null)
-			.setFooter({
-				text: `⬆️ ${d.score} | u/${d.author}`,
-			});
-
-		if (image) {
-			embed.setImage(image);
-		}
-		await channel.send({
-			embeds: [embed],
-		});
+		const embed = formatPostIntoEmbed(d);
+		embeds.push(embed);
 	}
+	return embeds;
+}
+
+function formatPostIntoEmbed(d: Data2) {
+	const image =
+		d.post_hint === "image"
+			? d.url
+			: d.preview?.images?.[0]?.source?.url?.replaceAll("&amp;", "&") || null;
+
+	const embed = new EmbedBuilder()
+		.setTitle(d.title)
+		.setURL(`https://reddit.com${d.permalink}`)
+		.setDescription(d.selftext ? d.selftext.slice(0, 2000) : null)
+		.setFooter({
+			text: `⬆️ ${d.score} | u/${d.author}`,
+		});
+
+	if (image) {
+		embed.setImage(image);
+	}
+	return embed;
 }
 
 async function fetchPosts() {
 	const url = "https://www.reddit.com/r/MiSideReddit/hot.json";
 
-	return (await fetch(url, {
+	const posts = (await fetch(url, {
 		headers: {
 			"User-Agent":
 				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36",
@@ -48,4 +66,10 @@ async function fetchPosts() {
 	})
 		.then((res) => res.json())
 		.catch(() => null)) as RedditData | null;
+	if (posts === null) {
+		Log.error("Failed fetching Reddit posts. More info below:");
+		Log.error(posts);
+	}
+
+	return posts;
 }
