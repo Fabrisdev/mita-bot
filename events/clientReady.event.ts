92ab444f5a42dc2ae4dc37f928ae850ea88f494b
fabrisdev <ezepuntocom@hotmail.com>
2026-02-22 14:10:53 -0300

    moved to safe area as we're sure client is ready there

--- events/clientReady.event.ts
+++ events/clientReady.event.ts
@@ -1,9 +1,13 @@
 import type { Client } from "discord.js";
 import { setupBirthdayIntervals } from "../birthdays";
+import { publishRedditPosts } from "../reddit";
 import { registerTempRoleTimeouts } from "../tempRolesHandler";
 
 export default async (client: Client<true>) => {
 	console.log(`Logged in as ${client.user.tag}!`);
 	await registerTempRoleTimeouts();
 	await setupBirthdayIntervals();
+	const TWELVE_HOURS = 12 * 60 * 60 * 1000;
+	setInterval(publishRedditPosts, TWELVE_HOURS);
+	await publishRedditPosts();
 };
--- index.ts
+++ index.ts
@@ -2,11 +2,7 @@ import { startApiService } from "./api";
 import { client } from "./client";
 import { token } from "./environment";
 import { registerEvents } from "./events/register";
-import { publishRedditPosts } from "./reddit";
 
 await registerEvents();
 client.login(token());
 startApiService();
-const TWELVE_HOURS = 12 * 60 * 60 * 1000;
-setTimeout(publishRedditPosts, TWELVE_HOURS);
-await publishRedditPosts();
