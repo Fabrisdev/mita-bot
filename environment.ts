bf6aa513842a3ea917dbd27c6e4cfcedbc8597f5
fabrisdev <ezepuntocom@hotmail.com>
2026-02-24 22:22:22 -0300

    updating client id

--- environment.ts
+++ environment.ts
@@ -7,31 +7,11 @@ export function token() {
 }
 
 export function clientId() {
-	const clientId = process.env.CLIENT_ID;
-	if (clientId === undefined) warnAndCrash("client id");
-	return clientId;
+	return isDevEnvironment ? "1476017873729228871" : "1461518757015851111";
 }
 
 export function guildId() {
 	return isDevEnvironment ? "1461519620660924543" : "1369433346546991136";
-}
-
-export function convexUrl() {
-	const convexUrl = process.env.CONVEX_URL;
-	if (convexUrl === undefined) warnAndCrash("convex url");
-	return convexUrl;
-}
-
-export function apiPort() {
-	const apiPort = process.env.API_PORT;
-	if (apiPort === undefined) warnAndCrash("api port");
-	return apiPort;
-}
-
-export function jwtSecret() {
-	const jwtSecret = process.env.JWT_SECRET;
-	if (jwtSecret === undefined) warnAndCrash("jwt secret");
-	return new TextEncoder().encode(jwtSecret);
 }
 
 function warnAndCrash(field: string): never {
