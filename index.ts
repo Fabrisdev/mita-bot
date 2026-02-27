e6b46e3f6f870d686fa1586af03dcce51a372701
fabrisdev <ezepuntocom@hotmail.com>
2026-02-27 13:03:28 -0300

    Making sure bot doesnt crash on error

--- index.ts
+++ index.ts
@@ -4,6 +4,14 @@ import { token } from "./environment";
 import { registerEvents } from "./events/register";
 import { Log } from "./log";
 
+process.on("unhandledRejection", (reason) => {
+	console.error(reason);
+});
+
+process.on("uncaughtException", (error) => {
+	console.error(error);
+});
+
 Log.log(isDevEnvironment ? "DEV mode enabled." : "Working on production.");
 await registerEvents();
 client.login(token());
