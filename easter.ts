ac1c244eb256c1cd6ad47a87af4978d3d6b6f4f5
fabrisdev <ezepuntocom@hotmail.com>
2026-04-05 19:53:27 -0300

    fix filename and client being accessed before ready

--- easter.ts
+++ easter.ts
@@ -1,22 +1,23 @@
 import path from "node:path";
+import type { Guild } from "discord.js";
 import { client } from "./client";
 import { guildId } from "./environment";
 import { cycle } from "./utils";
 
-const guild = await client.guilds.fetch(guildId());
 const images = cycle(["Cappie", "Kind", "Mila", "Mita", "Short"]);
 
-export function setupEasterEvent() {
+export async function setupEasterEvent() {
 	const FIFTEEN_MINUTES = 15 * 60 * 1000;
-	setInterval(postEgg, FIFTEEN_MINUTES);
+	const guild = await client.guilds.fetch(guildId());
+	setInterval(() => postEgg(guild), FIFTEEN_MINUTES);
 }
 
-function postEgg() {
+function postEgg(guild: Guild) {
 	const channel = guild.channels.cache
 		.filter((channel) => channel.isSendable())
 		.random();
 	if (channel === undefined) return;
-	const filename = `${images.next().value}.png`;
+	const filename = `Choco${images.next().value}.png`;
 	const imagePath = path.join("assets", filename);
 	channel.send({
 		files: [imagePath],
